23654de81adc63b927315f6c217dbc9261873fb0
gar6920 <gar6920@gmail.com>
2025-03-21 12:49:31 -0400

    4 screens working, need to get 3 and 2 with menu to select implementation and screen number at startup

--- server/core/server.js
+++ server/core/server.js
@@ -3,8 +3,73 @@
  * Entry point for the game server
  */
 
-// Import the server module
-const { gameServer } = require('./index');
+// Import the server module and implementation config
+const { gameServer, serverConfig, setActiveImplementation } = require('./index');
 
-// The server is automatically started in the imported module
-console.log('Server started through the modular architecture');
+// Simple implementation selector
+console.log('\n===== 3D AI Game Platform =====');
+console.log('Available implementations:');
+console.log('1. default' + (serverConfig.activeImplementation === 'default' ? ' (current)' : ''));
+console.log('2. numberblocks' + (serverConfig.activeImplementation === 'numberblocks' ? ' (current)' : ''));
+console.log('\nCurrent implementation: ' + serverConfig.activeImplementation);
+
+// Set up stdin to capture keypresses
+process.stdin.setRawMode(true);
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+let countdown = 10;
+let countdownTimer;
+let selected = false;
+
+console.log(`\nPress 1 or 2 to select an implementation`);
+console.log(`Starting with current implementation in ${countdown} seconds...`);
+
+// Start countdown
+countdownTimer = setInterval(() => {
+    countdown--;
+    
+    if (countdown <= 0) {
+        clearInterval(countdownTimer);
+        if (!selected) {
+            console.log(`\nUsing current implementation: ${serverConfig.activeImplementation}`);
+            console.log('Server running on http://localhost:3000');
+            process.stdin.setRawMode(false);
+            process.stdin.pause();
+        }
+    } else {
+        process.stdout.write(`\rStarting with current implementation in ${countdown} seconds...`);
+    }
+}, 1000);
+
+// Handle key presses
+process.stdin.on('data', (key) => {
+    // Check for implementation selection keys
+    if (key === '1') {
+        selected = true;
+        clearInterval(countdownTimer);
+        if (serverConfig.activeImplementation !== 'default') {
+            setActiveImplementation('default');
+            console.log('\nImplementation set to: default');
+        } else {
+            console.log('\nImplementation already set to: default');
+        }
+        console.log('Server running on http://localhost:3000');
+        process.stdin.setRawMode(false);
+        process.stdin.pause();
+    } else if (key === '2') {
+        selected = true;
+        clearInterval(countdownTimer);
+        if (serverConfig.activeImplementation !== 'numberblocks') {
+            setActiveImplementation('numberblocks');
+            console.log('\nImplementation set to: numberblocks');
+        } else {
+            console.log('\nImplementation already set to: numberblocks');
+        }
+        console.log('Server running on http://localhost:3000');
+        process.stdin.setRawMode(false);
+        process.stdin.pause();
+    } else if (key === '\u0003') { // Ctrl+C
+        process.exit();
+    }
+});
